fdde8a896942356eec513092995f0b0a25018f36
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-09-08 18:18:00 +0300

    Initial Commit

---
+++ Restart-App/Restart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_AppApp.swift
+//  Restart-App
+//
+//  Created by mac on 8.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Restart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
